89a26d503680831879f6d89230ac9c5597b05791
Arvin Lin <a7lin@ucsd.edu>
2020-12-24 10:20:17 -0800

    Reformat file

--- api/controller/UserRouter.ts
+++ api/controller/UserRouter.ts
@@ -1,95 +1,51 @@
 import express from 'express';
-import {ResponsePacket, User} from '../model/struct';
-import {getUser, putUser} from '../model/model';
+import {logger} from '../../index';
+import {UserPacket, ChangeResultPacket} from '../model/struct';
+import {dbDistributor} from '../model/model';
 import {sendClientUser, sendClientResult} from '../messenger/index';
 
-let router = express.Router();
+const router = express.Router();
 
 router.get('/:id', (req, res) => {
-    let error;
+  let error;
 
-    /* Check if the params query is valid */
-    if (isNaN(req.params.id))
-    {
-        res.end(400);
-        return;
-    }
+  /* Check if the params query is valid */
+  if (isNaN(parseInt(req.params.id))) {
+    res.end(400);
+    return;
+  }
 
-    /* Retrieve user object from database */
-    let userObj: ResponsePacket = {};
-    error = getUser(BigInt(req.params.id), userObj);
+  /* Retrieve user object from database */
+  const userObj: UserPacket = {} as UserPacket;
+  error = dbDistributor.getUser(req.params.id, userObj);
 
-    if (error != null)
-    {
-        res.end(500);
-        return;
-    }
+  if (error !== null) {
+    res.end(500);
+    return;
+  }
 
-    /* Send user data back to client */
-    sendClientUser(
-        userObj.packetUser,
-        res
-    );
+  /* Send user data back to client */
+  sendClientUser(userObj.data, res);
 
-    res.end();
+  res.end();
 });
 
-router.put('/create', (req, res) => {
-    let error;
+router.put('/', (req, res) => {
+  let error;
 
-    /* Check if the params query is valid */
-    if (isNaN(req.params.id))
-    {
-        res.end(400);
-        return;
-    }
+  /* Put user object to database */
+  const result: ChangeResultPacket = {} as ChangeResultPacket;
+  error = dbDistributor.putUser(req.body, result);
 
-    /* Put user object to database */
-    let result: ResponsePacket = {};
-    error = putUser(req.body, result);
+  if (error !== null) {
+    res.end(500);
+    return;
+  }
 
-    if (error != null)
-    {
-        res.end(500);
-        return;
-    }
+  /* Send user data back to client */
+  sendClientResult(result.data, res);
 
-    /* Send user data back to client */
-    sendClientResult(
-        result.packetResult,
-        res
-    );
-
-    res.end();
-});
-
-router.put('/update', (req, res) => {
-    let error;
-
-    /* Check if the params query is valid */
-    if (isNaN(req.params.id))
-    {
-        res.end(400);
-        return;
-    }
-
-    /* Put user object to database */
-    let result: ResponsePacket = {};
-    error = putUser(req.body, result);
-
-    if (error != null)
-    {
-        res.end(500);
-        return;
-    }
-
-    /* Send user data back to client */
-    sendClientResult(
-        result.packetResult,
-        res
-    );
-
-    res.end();
+  res.end();
 });
 
 export const UserRouter = router;
