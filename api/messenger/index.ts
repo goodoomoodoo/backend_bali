4a2a8659d896fa11820b9027c4ad721d2c556002
Arvin Lin <a7lin@ucsd.edu>
2020-12-24 10:02:58 -0800

    Change SendClient*() functions to return after status is sent

--- api/messenger/index.ts
+++ api/messenger/index.ts
@@ -2,35 +2,42 @@ import {User, Stash, Directory, FileEntry} from '../model/struct';
 import {Response} from 'express';
 
 export function sendClientResult(result: boolean, res: Response) {
-    let data = {'success': result};
-    res.send(JSON.stringify(data));
+  const data = {success: result};
+  res.send(JSON.stringify(data));
 }
 
-
 export function sendClientUser(data: User, res: Response) {
-    if (data === null)
-        res.sendStatus(404);
+  if (data === null) {
+    res.sendStatus(404);
+    return;
+  }
 
-    res.send(JSON.stringify(data));
+  res.send(JSON.stringify(data));
 }
 
 export function sendClientStash(data: Stash, res: Response) {
-    if (data === null)
-        res.sendStatus(404);
+  if (data === null) {
+    res.sendStatus(404);
+    return;
+  }
 
-    res.send(JSON.stringify(data));
+  res.send(JSON.stringify(data));
 }
 
 export function sendClientDirectory(data: Directory, res: Response) {
-    if (data === null)
-        res.sendStatus(404);
+  if (data === null) {
+    res.sendStatus(404);
+    return;
+  }
 
-    res.send(JSON.stringify(data));
+  res.send(JSON.stringify(data));
 }
 
 export function sendClientFileEntry(data: FileEntry, res: Response) {
-    if (data === null)
-        res.sendStatus(404);
-        
-    res.send(JSON.stringify(data));
+  if (data === null) {
+    res.sendStatus(404);
+    return;
+  }
+
+  res.send(JSON.stringify(data));
 }
