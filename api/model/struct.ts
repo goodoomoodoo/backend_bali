0e42b2fb8ac049825a1a345597639ac03d54c5f5
Arvin Lin <a7lin@ucsd.edu>
2020-12-31 19:59:58 -0800

    Add generic objects, update key props, and add instance checking function

--- api/model/struct.ts
+++ api/model/struct.ts
@@ -4,73 +4,146 @@
  * Note: version number always starts at 1, sent by client.
  */
 
+export const instanceOf = (keys: string[], data: DBObject) => {
+  for (let key of keys) {
+    if (!(key in data)) return false;
+  }
+
+  return true
+}
+
+export interface DBObject {
+  [hash: string]: number | string;
+}
+
 export interface UserPacket {
   data: User;
 }
 
 export interface StashPacket {
-  data: Stash;
+  data: Stash[];
 }
 
 export interface DirectoryPacket {
-  data: Directory;
+  data: ModelItem[];
 }
 
 export interface FileEntryPacket {
-  data: FileEntry;
+  data: FileEntry[];
 }
 
-export interface ChangeResultPacket {
-  data: boolean; // True if the change, such as put and delete, are succesful
+export interface Model extends DBObject {
+  id: string; // Primary Key
+  version: number;
+}
+
+export interface ModelItem extends Model {
+  owner: string; // User.id Foreign Key, cascade delete
+}
+
+export interface ModelFile extends ModelItem {
+  parent: string; // Stash.id | Directory.id Foriegn Key, cascade on delete
+  next: string; // ModelFile.id Recursive Foriegn Key
+  timestamp: string;
 }
 
 /**
  * User table
  */
-export interface User {
-  id: string; // Primary Key
+export interface User extends Model {
+  inventory: string; // Inventory.id Foreign Key
   firstName: string;
   lastName: string;
   email: string;
-  version: number;
 }
+
+export const UserPropsKey: string[] = [
+  'id',
+  'inventory',
+  'firstName',
+  'lastName',
+  'email',
+  'version',
+];
+
+/**
+ * Inventory table
+ * Inventory distinct to each user that stores all stashes own the user
+ */
+export interface Inventory extends ModelItem {
+  stashRoot: string; // Stash.id Foreign Key
+}
+
+export const InventoryPropsKey: string[] = [
+  'id',
+  'owner',
+  'stashRoot',
+  'version',
+];
 
 /**
  * Stash table
  * Stash are own by users, and each stash should store a files and directories
  */
-export interface Stash {
-  id: string; // Primary Key
-  owner: string; // User.id Foreign Key, cascade delete
+export interface Stash extends ModelItem {
+  next: string; // Stash.id Recursive Foreign Key
+  child: string; // Directory.id | FileEntry.id Foreign Key
   name: string;
   createdDate: string;
-  version: number;
 }
+
+export const StashPropsKey: string[] = [
+  'id',
+  'owner',
+  'next',
+  'child',
+  'name',
+  'createdDate',
+  'version',
+];
 
 /**
  * Directory table
  * Directory stores the files and sub directory
  */
-export interface Directory {
-  id: string; // Primary Key
-  owner: string; // User.id Foreign Key, cascade delete
+export interface Directory extends ModelFile {
   stash: string; // Stash.id Foreign Key, cascade delete
+  child: string; // Directory.id | FileEntry.id Recursive Foreign Key
   name: string;
   createdDate: string;
-  timestamp: string;
-  version: number;
 }
+
+export const DirectoryPropsKey: string[] = [
+  'id',
+  'owner',
+  'stash',
+  'parent',
+  'child',
+  'next',
+  'name',
+  'createdDate',
+  'timestamp',
+  'version',
+];
 
 /**
  * FileEntry table
  */
-export interface FileEntry {
-  id: string; // Primary Key
-  owner: string; // Directory.id Foreign Key, cascade delete
-  directory: string; // Directory.id Foreign Key, cascade delete
+export interface FileEntry extends ModelFile {
+  stash: string; // Stash.id Foreign Key, cascade delete
   name: string;
   content: string;
   createdDate: string;
-  timestamp: string;
-  version: number;
 }
+
+export const FileEntryPropsKey: string[] = [
+  'id',
+  'owner',
+  'stash',
+  'next',
+  'name',
+  'content',
+  'createdDate',
+  'timeStamp',
+  'version',
+];
