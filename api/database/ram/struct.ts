d2450d8ef30177e9a7ce79d033f2156969c541ca
Arvin Lin <a7lin@ucsd.edu>
2020-12-31 19:55:40 -0800

    Update store structure and hierarchy

--- api/database/ram/struct.ts
+++ api/database/ram/struct.ts
@@ -2,12 +2,23 @@
  * sturct.js stores the local store structure
  */
 
-import {User} from '../../model/struct';
+import {Stash, User, Inventory, Model, ModelFile} from '../../model/struct';
 
 export interface Store {
   users: UserStore;
+  inventories: InventoryStore;
+  stashes: StashStore;
+  fileTree: treeStore;
 }
 
-interface UserStore {
-  [hash: string]: User;
-}
+export interface ModelStore<T extends Model> {
+  [hash: string]: T;
+} 
+
+export interface UserStore extends ModelStore<User>{}
+
+export interface InventoryStore extends ModelStore<Inventory>{}
+
+export interface StashStore extends ModelStore<Stash> {}
+
+export interface treeStore extends ModelStore<ModelFile>{}
