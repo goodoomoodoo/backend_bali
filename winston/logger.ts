86ee3bc2aeb8e6341bca1352f72fc4ca59f4eb02
Arvin Lin <a7lin@ucsd.edu>
2021-01-02 18:49:51 -0800

    Add test env to logger

--- winston/logger.ts
+++ winston/logger.ts
@@ -4,40 +4,42 @@ export class LoggerWrapper {
   write: winston.Logger;
 
   constructor(level: string, env: string, timeFmt: string) {
-    this.write = winston.createLogger({
-      level: level,
-      format: winston.format.combine(
-        winston.format.timestamp({
-          format: timeFmt,
-        }),
-        winston.format.errors({stack: true}),
-        winston.format.splat(),
-        winston.format.json()
-      ),
-      defaultMeta: {service: 'Bali REST Endpoint'},
-      transports: [
-        new winston.transports.File({
-          filename: 'bali-std-err.log',
-          level: 'error',
-        }),
-        new winston.transports.File({filename: 'bali-std-out.log'}),
-      ],
-    });
+    if (env !== 'test') {
+      this.write = winston.createLogger({
+        level: level,
+        format: winston.format.combine(
+          winston.format.timestamp({
+            format: timeFmt,
+          }),
+          winston.format.errors({stack: true}),
+          winston.format.splat(),
+          winston.format.json()
+        ),
+        defaultMeta: {service: 'Bali REST Endpoint'},
+        transports: [
+          new winston.transports.File({
+            filename: 'bali-std-err.log',
+            level: 'error',
+          }),
+          new winston.transports.File({filename: 'bali-std-out.log'}),
+        ],
+      });
+    } else {
+      this.write = winston.createLogger({level: level});
+    }
 
     /* Add realtime log format if not production mode */
     if (env !== 'production') {
-      if (process.env.NODE_ENV !== 'production') {
-        this.write.add(
-          new winston.transports.Console({
-            format: winston.format.combine(
-              winston.format.colorize(),
-              winston.format.printf(
-                info => `${info.timestamp} [${info.level}]: ${info.message}`
-              )
-            ),
-          })
-        );
-      }
+      this.write.add(
+        new winston.transports.Console({
+          format: winston.format.combine(
+            winston.format.colorize(),
+            winston.format.printf(
+              info => `${info.timestamp} [${info.level}]: ${info.message}`
+            )
+          ),
+        })
+      );
     }
   }
 
